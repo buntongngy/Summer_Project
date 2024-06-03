f4f02431cc7359cb1a480c9a0af62cb22567aa85
buntongngy <144354284+buntongngy@users.noreply.github.com>
2024-06-03 12:48:34 +0700

    Adjust the tile btn in the bottom bar,

--- src/manager/TileManager.java
+++ src/manager/TileManager.java
@@ -10,9 +10,16 @@ import java.util.ArrayList;
 public class TileManager {
 
     public Tile GRASS, WATER, BL_Water_Corner, BR_Water_Corner, TL_Water_Corner, TR_Water_Corner, L_Water, R_Water, T_Water, B_Water,
-    ROAD, ROAD_LR, ROAD_TB, ROAD_B_TO_R, ROAD_L_TO_B, ROAD_L_TO_T, ROAD_T_TO_R, TL_ISLE, TR_ISLE, BR_ISLE, BL_ISLE;
+     ROAD_LR, ROAD_TB, ROAD_B_TO_R, ROAD_L_TO_B, ROAD_L_TO_T, ROAD_T_TO_R, TL_ISLE, TR_ISLE, BR_ISLE, BL_ISLE;
     public BufferedImage atlas;
+
     public ArrayList<Tile> tiles = new ArrayList<>();
+    public ArrayList<Tile> roadS = new ArrayList<>();
+    public ArrayList<Tile> roadC = new ArrayList<>();
+    public ArrayList<Tile> corner = new ArrayList<>();
+    public ArrayList<Tile> beach = new ArrayList<>();
+    public ArrayList<Tile> island = new ArrayList<>();
+
 
 
     public TileManager() {
@@ -24,31 +31,40 @@ public class TileManager {
 
     private void createTiles() {
         int id = 0;
+
         tiles.add(GRASS = new Tile(getSprite(9,0), id++, "Grass"));
         tiles.add(WATER = new Tile(getSprite(1,0), id++ , "Water"));
-        tiles.add(ROAD = new Tile(getSprite(8,0), id++, "Road"));
+        tiles.add(ROAD_LR = new Tile(getSprite(8,0), id++, "Road"));
 
-        tiles.add(ROAD_TB = new Tile(imgFix.getRotateImg(getSprite(8,0), 90), id++, "TB_Road"));
-        tiles.add(ROAD_B_TO_R = new Tile(getSprite(7,0), id++, "Road_Bottom_To_Right"));
+        roadS.add(ROAD_TB = new Tile(imgFix.getRotateImg(getSprite(8,0), 90), id++, "TB_Road"));
+        roadS.add(ROAD_LR = new Tile(getSprite(8,0), id++ ,"ROAD_LR"));
 
-        tiles.add(ROAD_L_TO_B = new Tile(imgFix.getRotateImg(getSprite(7,0), 90), id++, "Road_Left_To_Bottom"));
-        tiles.add(ROAD_L_TO_T = new Tile(imgFix.getRotateImg(getSprite(7,0), 180), id++, "Road_Left_To_Top"));
-        tiles.add(ROAD_T_TO_R = new Tile(imgFix.getRotateImg(getSprite(7,0), 270), id++, "Road_Top_To_Right"));
+        roadC.add(ROAD_B_TO_R = new Tile(getSprite(7,0), id++, "Road_Bottom_To_Right"));
+        roadC.add(ROAD_L_TO_B = new Tile(imgFix.getRotateImg(getSprite(7,0), 90), id++, "Road_Left_To_Bottom"));
+        roadC.add(ROAD_L_TO_T = new Tile(imgFix.getRotateImg(getSprite(7,0), 180), id++, "Road_Left_To_Top"));
+        roadC.add(ROAD_T_TO_R = new Tile(imgFix.getRotateImg(getSprite(7,0), 270), id++, "Road_Top_To_Right"));
 
-        tiles.add(BL_Water_Corner = new Tile(imgFix.buildImg(getImg(0,0,5,0)), id++, "BL_Water_Corner"));
-        tiles.add(BR_Water_Corner = new Tile(imgFix.getBuildRotImg(getImg(0,0,5,0), 270, 1), id++, "BR_Water_Corner"));
-        tiles.add(TL_Water_Corner = new Tile(imgFix.getBuildRotImg(getImg(0,0,5,0), 90, 1), id++, "TL_Water_Corner"));
-        tiles.add(TR_Water_Corner = new Tile(imgFix.getBuildRotImg(getImg(0,0,5,0), 180, 1), id++, "TR_Water_Corner"));
+        corner.add(BL_Water_Corner = new Tile(imgFix.buildImg(getImg(0,0,5,0)), id++, "BL_Water_Corner"));
+        corner.add(BR_Water_Corner = new Tile(imgFix.getBuildRotImg(getImg(0,0,5,0), 270, 1), id++, "BR_Water_Corner"));
+        corner.add(TL_Water_Corner = new Tile(imgFix.getBuildRotImg(getImg(0,0,5,0), 90, 1), id++, "TL_Water_Corner"));
+        corner.add(TR_Water_Corner = new Tile(imgFix.getBuildRotImg(getImg(0,0,5,0), 180, 1), id++, "TR_Water_Corner"));
 
-        tiles.add(T_Water = new Tile(imgFix.buildImg(getImg(0,0,6,0)), id++, "T_Water"));
-        tiles.add(R_Water = new Tile(imgFix.getBuildRotImg(getImg(0,0,6,0), 90, 1), id++, "R_Water"));
-        tiles.add(B_Water = new Tile(imgFix.getBuildRotImg(getImg(0,0,6,0), 180, 1), id++, "B_Water"));
-        tiles.add(L_Water = new Tile(imgFix.getBuildRotImg(getImg(0,0,6,0), 270, 1), id++, "L_Water"));
+        beach.add(T_Water = new Tile(imgFix.buildImg(getImg(0,0,6,0)), id++, "T_Water"));
+        beach.add(R_Water = new Tile(imgFix.getBuildRotImg(getImg(0,0,6,0), 90, 1), id++, "R_Water"));
+        beach.add(B_Water = new Tile(imgFix.getBuildRotImg(getImg(0,0,6,0), 180, 1), id++, "B_Water"));
+        beach.add(L_Water = new Tile(imgFix.getBuildRotImg(getImg(0,0,6,0), 270, 1), id++, "L_Water"));
 
-        tiles.add(TL_ISLE = new Tile(imgFix.buildImg(getImg(0,0,4,0)), id++, "TL_Isle"));
-        tiles.add(TR_ISLE = new Tile(imgFix.getBuildRotImg(getImg(0,0,4,0), 90, 1), id++, "TR_Isle"));
-        tiles.add(BR_ISLE = new Tile(imgFix.getBuildRotImg(getImg(0,0,4,0), 180, 1), id++, "BR_Isle"));
-        tiles.add(BL_ISLE = new Tile(imgFix.getBuildRotImg(getImg(0,0,4,0), 270, 1), id++, "BL_Isle"));
+        island.add(TL_ISLE = new Tile(imgFix.buildImg(getImg(0,0,4,0)), id++, "TL_Isle"));
+        island.add(TR_ISLE = new Tile(imgFix.getBuildRotImg(getImg(0,0,4,0), 90, 1), id++, "TR_Isle"));
+        island.add(BR_ISLE = new Tile(imgFix.getBuildRotImg(getImg(0,0,4,0), 180, 1), id++, "BR_Isle"));
+        island.add(BL_ISLE = new Tile(imgFix.getBuildRotImg(getImg(0,0,4,0), 270, 1), id++, "BL_Isle"));
+
+        tiles.addAll(roadS);
+        tiles.addAll(roadC);
+        tiles.addAll(corner);
+        tiles.addAll(beach);
+        tiles.addAll(island);
+
     }
 
     private BufferedImage[] getImg(int firstX, int firstY, int secondX, int secondY) {
@@ -71,5 +87,25 @@ public class TileManager {
 
     private BufferedImage getSprite(int xCord, int yCord) {
         return atlas.getSubimage(xCord * 32, yCord * 32, 32,32);
+    }
+
+    public ArrayList<Tile> getBeach() {
+        return beach;
+    }
+
+    public ArrayList<Tile> getCorner() {
+        return corner;
+    }
+
+    public ArrayList<Tile> getIsland() {
+        return island;
+    }
+
+    public ArrayList<Tile> getRoadC() {
+        return roadC;
+    }
+
+    public ArrayList<Tile> getRoadS() {
+        return roadS;
     }
 }
--- src/ui/ToolBar.java
+++ src/ui/ToolBar.java
@@ -7,6 +7,8 @@ import screen.Editing;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static main.GameState.MENU;
 import static main.GameState.setGameState;
@@ -17,7 +19,9 @@ public class ToolBar extends Bar{
     private buttonClass bMenu, bSave;
     private Tile selectedTile;
 
-    private ArrayList<buttonClass> tileBtn = new ArrayList<>();
+    //private ArrayList<buttonClass> tileBtn = new ArrayList<>();
+    private Map<buttonClass, ArrayList<Tile>> map = new HashMap<buttonClass, ArrayList<Tile>>();
+    private buttonClass bGrass, bWater, bRoadS, bRoadC, bWaterC, bWaterB, bWaterI;
 
     public ToolBar(int x, int y, int width, int height, Editing editing) {
         super(x, y, width, height);
@@ -36,10 +40,25 @@ public class ToolBar extends Bar{
         int xOffset = (int) (w * 1.1);
         int i = 0;
 
-        for (Tile tile : editing.getGame().getTileManager().tiles) {
-            tileBtn.add(new buttonClass(tile.getName(), xStart + xOffset * i, yStart, w, h, i));
-            i++;
-        }
+        bGrass = new buttonClass("Grass", xStart, yStart, w, h ,i++);
+        bWater = new buttonClass("Water", xStart + xOffset, yStart, w, h, i++);
+
+        initMapBtn(bRoadS, editing.getGame().getTileManager().getRoadS(), xStart, yStart, xOffset, w, h, i++);
+        initMapBtn(bRoadC, editing.getGame().getTileManager().getRoadC(), xStart, yStart, xOffset, w, h, i++);
+        initMapBtn(bWaterC, editing.getGame().getTileManager().getCorner(), xStart, yStart, xOffset, w, h, i++);
+        initMapBtn(bWaterB, editing.getGame().getTileManager().getBeach(), xStart, yStart, xOffset, w, h, i++);
+        initMapBtn(bWaterI, editing.getGame().getTileManager().getIsland(), xStart, yStart, xOffset, w, h, i++);
+
+
+    }
+
+    private void initMapBtn(buttonClass b, ArrayList<Tile> list, int x, int y, int xOff, int w, int h, int id) {
+
+        b = new buttonClass("", x + xOff * id, y, w, h, id);
+        map.put(b, list);
+
+
+
     }
 
     private void saveLevel() {
@@ -58,38 +77,53 @@ public class ToolBar extends Bar{
     private void drawBtn(Graphics g) {
         bMenu.draw(g);
         bSave.draw(g);
-        drawTileBtn(g);
+
+
+        drawNormalBtn(g, bGrass);
+        drawNormalBtn(g, bWater);
         drawSelectTile(g);
+        drawMapBtn(g);
+    }
+
+    private void drawNormalBtn(Graphics g, buttonClass b) {
+
+        g.drawImage(getBtnImg(b.getId()), b.x, b.y, b.width, b.height, null);
+        drawBtnFeedBack(g, b);
+    }
+
+    private void drawMapBtn(Graphics g) {
+        for(Map.Entry<buttonClass, ArrayList<Tile>> entry : map.entrySet()) {
+            buttonClass b = entry.getKey();
+            BufferedImage img = entry.getValue().get(0).getSprite();
+
+            g.drawImage(img, b.x, b.y, b.width, b.height, null);
+            drawBtnFeedBack(g, b);
+
+        }
+    }
+
+    private void drawBtnFeedBack(Graphics g, buttonClass b) {
+        // Mouse Over
+        if (b.isMouseOver()) {
+            g.setColor(Color.GREEN);
+        } else {
+            g.setColor(Color.YELLOW);
+        }
+
+        // Border
+        g.drawRect(b.x, b.y, b.width, b.height);
+
+        // Mouse Press
+        if (b.isMousePress()) {
+            g.drawRect(b.x + 1, b.y + 1, b.width - 2, b.height - 2);
+            g.drawRect(b.x + 2, b.y + 2, b.width - 4, b.height - 4);
+        }
     }
 
     private void drawSelectTile(Graphics g) {
         if (selectedTile != null) {
             g.drawImage(selectedTile.getSprite(), 550, 640, 50, 50, null);
             g.drawRect(550, 640, 50, 50);
-        }
-    }
-
-    private void drawTileBtn(Graphics g) {
-        for (buttonClass b : tileBtn) {
-
-            // Image sprite
-            g.drawImage(getBtnImg(b.getId()), b.x, b.y, b.width, b.height, null);
-
-            // Mouse Over
-            if (b.isMouseOver()) {
-                g.setColor(Color.GREEN);
-            } else {
-                g.setColor(Color.YELLOW);
-            }
-
-            // Border
-            g.drawRect(b.x, b.y, b.width, b.height);
-
-            // Mouse Press
-            if (b.isMousePress()) {
-                g.drawRect(b.x + 1, b.y + 1, b.width - 2, b.height - 2);
-                g.drawRect(b.x + 2, b.y + 2, b.width - 4, b.height - 4);
-            }
         }
     }
 
@@ -102,30 +136,47 @@ public class ToolBar extends Bar{
             setGameState(MENU);
         } else if (bSave.getBounds().contains(x, y)) {
             saveLevel();
-        } else {
-            for (buttonClass b : tileBtn) {
+        } else if (bWater.getBounds().contains(x,y)) {
+            selectedTile = editing.getGame().getTileManager().getTile(bWater.getId());
+            editing.setSelectedTile(selectedTile);
+            return;
+        } else if (bGrass.getBounds().contains(x,y)) {
+            selectedTile = editing.getGame().getTileManager().getTile(bGrass.getId());
+            editing.setSelectedTile(selectedTile);
+            return;
+        }  else {
+
+            for(buttonClass b : map.keySet()) {
                 if (b.getBounds().contains(x, y)) {
-                    selectedTile = editing.getGame().getTileManager().getTile(b.getId());
+                    selectedTile = map.get(b).get(0);
                     editing.setSelectedTile(selectedTile);
                     return;
                 }
             }
+
         }
     }
 
     public void mouseMoved(int x, int y) {
         bMenu.setMouseOver(false);
         bSave.setMouseOver(false);
+        bWater.setMouseOver(false);
+        bGrass.setMouseOver(false);
 
-        for (buttonClass b : tileBtn)
-            b.setMouseOver(false);
+        for(buttonClass b : map.keySet()) {
+            b.setMouseOver(true);
+        }
 
         if (bMenu.getBounds().contains(x, y)) {
             bMenu.setMouseOver(true);
         } else if (bSave.getBounds().contains(x, y)) {
             bSave.setMouseOver(true);
+        } else if (bGrass.getBounds().contains(x, y)) {
+            bGrass.setMouseOver(true);
+        }else if (bWater.getBounds().contains(x, y)) {
+            bWater.setMouseOver(true);
         }else {
-            for (buttonClass b : tileBtn) {
+            for (buttonClass b : map.keySet()) {
                 if (b.getBounds().contains(x, y)) {
                     b.setMouseOver(true);
                     return;
@@ -139,8 +190,12 @@ public class ToolBar extends Bar{
             bMenu.setMousePress(true);
         } else if(bSave.getBounds().contains(x, y)) {
             bSave.setMousePress(true);
+        }else if (bGrass.getBounds().contains(x, y)) {
+            bGrass.setMousePress(true);
+        }else if (bWater.getBounds().contains(x, y)) {
+            bWater.setMousePress(true);
         }else {
-            for (buttonClass b : tileBtn) {
+            for (buttonClass b : map.keySet()) {
                 if (b.getBounds().contains(x, y)) {
                     b.setMousePress(true);
                     return;
@@ -152,7 +207,9 @@ public class ToolBar extends Bar{
     public void mouseRelease(int x, int y) {
         bMenu.resetBoolean();
         bSave.resetBoolean();
-        for (buttonClass b : tileBtn) {
+        bWater.resetBoolean();
+        bGrass.resetBoolean();
+        for (buttonClass b : map.keySet()) {
             b.resetBoolean();
         }
     }
